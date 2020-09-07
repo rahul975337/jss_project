deec56dc0423575c3537a6680465165e79dd3141
rahul975337 <tilwani03@gmail.com>
2020-09-07 13:41:08 +0530

    added constants file

---
+++ lib/screens/home/home.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  double _safePaddingTop, _safePaddingBottom, _height, _width, _keyboard;
+  @override
+  Widget build(BuildContext context) {
+    _safePaddingTop = MediaQuery.of(context).padding.top;
+    _safePaddingBottom = MediaQuery.of(context).padding.bottom;
+    _width = MediaQuery.of(context).size.width;
+    _height = MediaQuery.of(context).size.height -
+        (_safePaddingBottom + _safePaddingTop);
+    _keyboard = MediaQuery.of(context).viewInsets.bottom;
+    return Container(
+      height: _height * 0.5,
+      width: _width * 0.75,
+    );
+  }
+}
