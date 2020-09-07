3101e5fd54b2d562f3d99d3d61e1205c71ed97f0
rahul975337 <tilwani03@gmail.com>
2020-09-07 11:47:26 +0530

    first commit

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+////Colours for dark theme/////
+const Color kDarkPrimaryBackgroundColor = Color(0xFF0b0c21);
+const Color kDarkSecondaryBackgroundColor = Color(0xFF121212);
+const Color kDarkAccentColour = Color(0xFF00ADBD);
+const Color kDarkTextColour = Color(0xFFFFFFFF);
+const Color kDarkComponentShadowColour = Color(0xCCFFFFFF);
